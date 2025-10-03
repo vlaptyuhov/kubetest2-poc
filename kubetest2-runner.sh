8114876c4d09f9a518f5d8ee2090e58327ff55d6
vlaptyuhov <47592468+vlaptyuhov@users.noreply.github.com>
2025-10-03 08:22:04 +0200

    Create kubetest2-runner.sh

---
+++ kubetest2-runner.sh
@@ -0,0 +1,9 @@
+echo "Running the script with kubetest2"
+
+kubetest2
+
+echo "Running step 1 - setup cluster and run pre-test"
+echo "Running step 2 - run tests"
+echo "Running step 3 - run post-test"
+
+echo "Finished Running the script with kubetest2"
